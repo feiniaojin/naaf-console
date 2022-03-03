7229acb8503e7974b743dd14a47206b153349356
qinyujie3 <qinyujie3@jd.com>
2022-03-03 21:59:54 +0800

    init

--- naaf-console-service/src/main/java/com/feiniaojin/naaf/console/service/SysUserAggregateRepository.java
+++ naaf-console-service/src/main/java/com/feiniaojin/naaf/console/service/SysUserAggregateRepository.java
@@ -1,11 +1,23 @@
 package com.feiniaojin.naaf.console.service;
 
+import com.feiniaojin.naaf.console.repository.SysUserRelRoleRepository;
+import com.feiniaojin.naaf.console.repository.SysUserRepository;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 public class SysUserAggregateRepository {
 
+    @Resource
+    private SysUserRepository userRepository;
+
+    @Resource
+    private SysUserRelRoleRepository relRoleRepository;
+
     public void saveUpdate(SysUserAggregate aggregate) {
+
+        String uid = aggregate.getEntity().getUid();
 
     }
 }
