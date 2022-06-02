eb1a7469540fcf85094e00601ee9d83227b32e8d
qinyujie3 <qinyujie3@jd.com>
2022-06-02 18:40:44 +0800

    init

--- naaf-console-commons/src/main/java/com/feiniaojin/naaf/console/commons/NaafDateUtil.java
+++ naaf-console-commons/src/main/java/com/feiniaojin/naaf/console/commons/NaafDateUtil.java
@@ -1,6 +1,5 @@
 package com.feiniaojin.naaf.console.commons;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
--- naaf-console-service/src/main/java/com/feiniaojin/naaf/console/sys/resource/ResourceAggregateRepository.java
+++ naaf-console-service/src/main/java/com/feiniaojin/naaf/console/sys/resource/ResourceAggregateRepository.java
@@ -3,14 +3,14 @@ package com.feiniaojin.naaf.console.sys.resource;
 import com.feiniaojin.naaf.console.data.SysResource;
 import com.feiniaojin.naaf.console.mapper.SysResourceMapper;
 import com.feiniaojin.naaf.console.repository.SysResourceRepository;
-import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 /**
  * SysResourceModel的定位是承接业务逻辑，model中不允许调用数据库、缓存
  */
-@Data
+@Component
 public class ResourceAggregateRepository {
 
     @Resource
